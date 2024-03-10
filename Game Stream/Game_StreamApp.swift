fef54f1ef0bb6113a4be9e863e0bab02008f86c9
s <as>
2024-03-10 15:25:08 -0400

    Initial Commit

---
+++ Game Stream/Game_StreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_StreamApp.swift
+//  Game Stream
+//
+//  Created by Angel alberto Bernechea on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Game_StreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
